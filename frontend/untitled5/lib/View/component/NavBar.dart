237e5f907d537fc780d04c1df6a6324ac9a98621
islem <islemberriche527@gmail.com>
2023-12-06 23:52:25 +0100

    navbarV1

--- frontend/untitled5/lib/View/component/NavBar.dart
+++ frontend/untitled5/lib/View/component/NavBar.dart
@@ -3,6 +3,7 @@ import 'package:untitled5/View/User/Login.dart';
 import '../../Model/user.dart';
 import '../../Services/user/UserService.dart';
 import '../User/ProfilePage.dart';
+import '../offer/OfferListWidget.dart';
 import '../offer/addOffer.dart';
 
 class NavBar extends StatefulWidget {
@@ -11,45 +12,18 @@ class NavBar extends StatefulWidget {
   @override
   State<NavBar> createState() => _NavBarState();
 }
-
 class _NavBarState extends State<NavBar> {
-
   Future<void> _onItemTapped(int index, BuildContext context) async {
-try{
-  print('Tapped index: $index');
+    try {
 
-
-    if (index == 1) {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => AddOfferScreen()),
-      );
+       if (index == 1) {
+        // Show the navigation menu for "Profil" options
+        showNavigationMenu(context);
+      }
+    } catch (e) {
+      print('Error: $e');
+      // Handle the error, e.g., show a toast or navigate to an error page.
     }
-    else if (index ==2)   {
-      print('Fetching user data...');
-      User user = await getUserByEmail();
-      print('Retrieved user: $user');
-      print('Navigating to ProfilePage');
-
-      Navigator.push(
-
-        context,
-
-      MaterialPageRoute(builder: (context) => ProfilePage(user: user)),
-      );
-    }
-    else if (index ==3)   {
-      logout();
-      Navigator.push(
-
-        context,
-        MaterialPageRoute(builder: (context) => LoginPage()),
-      );
-    }
-} catch (e) {
-  print('Error: $e');
-  // Handle the error, e.g., show a toast or navigate to an error page.
-}
   }
 
   @override
@@ -60,22 +34,93 @@ try{
           icon: Icon(Icons.home),
           label: 'Accueil',
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.add_circle),
-          label: 'Ajouter',
-        ),
+
         BottomNavigationBarItem(
           icon: Icon(Icons.person),
           label: 'Profil',
         ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.exit_to_app), // Icon for the logout button
-          label: 'Déconnexion', // Label for the logout button
-        )
+
       ],
       selectedItemColor: Colors.teal,
       unselectedItemColor: Colors.teal,
       onTap: (index) => _onItemTapped(index, context),
+    );
+  }
+
+  void showNavigationMenu(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Editer Mon Profil'),
+
+              onTap: () async {
+                User user = await getUserByEmail();
+
+                Navigator.pop(context); // Close the menu
+
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ProfilePage(user:user),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.list),
+              title: Text('Mes Offres'),
+              onTap: () {
+                Navigator.pop(context); // Close the menu
+                // Navigate to Mes Offres page
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => OfferListWidget(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.add),
+              title: Text('Ajouter Offre'),
+              onTap: () {
+
+                Navigator.pop(context); // Close the menu
+                // Navigate to Ajouter Offre page
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AddOfferScreen(),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text('déconnexion'),
+              onTap: () {
+
+                Navigator.pop(context); // Close the menu
+                logout();
+
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => LoginPage(),
+                  ),
+                );
+              },
+            ),
+
+
+          ],
+        );
+      },
     );
   }
 }
